41a01bfa15403846dcd952cbc4b69b72bc5cb002
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-11-23 08:13:07 +0900

    Update my plugin .php

--- wordpress/html/wp-content/plugins/hskwakr-practice/hskwakr-practice.php
+++ wordpress/html/wp-content/plugins/hskwakr-practice/hskwakr-practice.php
@@ -2,3 +2,14 @@
 /**
  * @package hskwakr-practice
  */
+
+/**
+Plugin Name: hskwakr practice
+Plugin URI: https://github.com/hskwakr/wp-plugin-practice
+Description: This is my first attempt on writing a custom plugin.
+Version: 1.0.0
+Author: hskwakr
+Author URI: https://github.com/hskwakr
+License: MIT
+Text Domain: hskwakr-practice
+ */
