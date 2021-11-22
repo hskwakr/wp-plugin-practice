8ac3dbeede35eb2b70b335b5567c39bb5848746b
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-11-23 08:19:43 +0900

    Update plugin

--- wordpress/html/wp-content/plugins/hskwakr-practice/hskwakr-practice.php
+++ wordpress/html/wp-content/plugins/hskwakr-practice/hskwakr-practice.php
@@ -3,7 +3,7 @@
  * @package hskwakr-practice
  */
 
-/**
+/*
 Plugin Name: hskwakr practice
 Plugin URI: https://github.com/hskwakr/wp-plugin-practice
 Description: This is my first attempt on writing a custom plugin.
@@ -12,4 +12,28 @@ Author: hskwakr
 Author URI: https://github.com/hskwakr
 License: MIT
 Text Domain: hskwakr-practice
- */
+*/
+
+/*
+MIT License
+
+Copyright (c) 2021 Hosokawa
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
