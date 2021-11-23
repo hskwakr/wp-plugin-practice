0b451221ed056045873687e2e15a1f7bb00345a1
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-11-24 07:38:26 +0900

    Add uninstall script

---
+++ wordpress/html/wp-content/plugins/hskwakr-practice/uninstall.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Trigger this file on Plugin uninstall
+ *
+ * @package hskwakr-practice
+ */
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+  die;
+}
+
+// Clear Database stored data
