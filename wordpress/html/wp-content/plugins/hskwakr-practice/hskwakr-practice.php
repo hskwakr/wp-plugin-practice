ceb3bc214b52b116c99d64591b4485ccbe23c6f3
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-11-24 17:36:36 +0900

    Update plugin

--- wordpress/html/wp-content/plugins/hskwakr-practice/hskwakr-practice.php
+++ wordpress/html/wp-content/plugins/hskwakr-practice/hskwakr-practice.php
@@ -47,13 +47,17 @@ defined( 'ABSPATH' ) or die( 'Cannot access this page.' );
  */
 class HskwakrPractice
 {
+  public  $plugin;
+
   public function __construct()
   {
+    $this->plugin = plugin_basename( __FILE__ );
   }
 
   function register()
   {
     add_action( 'admin_enque_scripts', array( $this, 'enqueue' ) );
+    add_action( 'admin_menu', array( $this, 'add_admin_page' ) );
   }
   
   function activate()
@@ -70,6 +74,24 @@ class HskwakrPractice
     flush_rewrite_rules();
   }
 
+  function enqueue()
+  {
+    // enqueue all our scripts
+    wp_enqueue_style( 'mypluginstyle', plugins_url( '/assets/mystyle.css', __FILE__ ) );
+    wp_enqueue_script( 'mypluginscript', plugins_url( '/assets/myscript.js', __FILE__ ) );
+  }
+
+  function add_admin_page()
+  {
+    add_menu_page( 'hskwakr practice', 'hskwakr', 'manage_options', 'hskwakr_practice', array( $this, 'admin_index' ), 'dashicons-store', 110 );
+  }
+
+  function admin_index()
+  {
+    require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';
+  }
+  
+  
   function create_post_type()
   {
     add_action( 'init', array( $this, 'custom_post_type' ) );
@@ -78,13 +100,6 @@ class HskwakrPractice
   function custom_post_type()
   {
     register_post_type( 'book', ['public' => true, 'label' => 'Books'] );
-  }
-
-  function enqueue()
-  {
-    // enqueue all our scripts
-    wp_enqueue_style( 'mypluginstyle', plugins_url( '/assets/mystyle.css', __FILE__ ) );
-    wp_enqueue_script( 'mypluginscript', plugins_url( '/assets/myscript.js', __FILE__ ) );
   }
 }
 
