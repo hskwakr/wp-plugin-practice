1f0d8ca2777259effdedd13df92af0adecde579e
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-11-26 10:53:34 +0900

    Add Init.php

---
+++ wordpress/html/wp-content/plugins/hskwakr-practice/inc/Init.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package hskwakr-practice
+ */
+namespace Inc;
+
+/**
+ * Class Init
+ * @author hskwakr
+ */
+final class Init
+{
+  /**
+   * Store all the classes inside an array
+   * @return array Full list of classes
+   */
+  public static function get_services()
+  {
+    return [
+      Pages\Admin::class
+    ];
+  }
+  
+  
+  /**
+   * Loop through the classes, initialize them,
+   * and call the register method if it exists
+   * @return
+   */
+  public static function register_services()
+  {
+    foreach ( self::get_services() as $class ) {
+      $service = self::instantiate( $class );
+
+      if ( method_exists( $service, 'register' ) ) {
+        $service->register();
+      }
+    }
+  }
+
+  /**
+   * Initialize the class
+   * @param $class class from the services array
+   * @return class instance
+   */
+  private static function instantiate( $class )
+  {
+    return new $class();
+  }
+}
