b7e0fb4a3f0f143f5d74b152af6422305a5694ad
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-11-26 09:10:37 +0900

    Update plugin

--- wordpress/html/wp-content/plugins/hskwakr-practice/hskwakr-practice.php
+++ wordpress/html/wp-content/plugins/hskwakr-practice/hskwakr-practice.php
@@ -41,6 +41,11 @@ SOFTWARE.
 // Protect from illegal access
 defined( 'ABSPATH' ) or die( 'Cannot access this page.' );
 
+
+if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
+  require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+}
+
 /**
  * Class HskwakrPractice
  * @author hskwakr
