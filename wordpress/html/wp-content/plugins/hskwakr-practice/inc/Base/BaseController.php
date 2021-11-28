86b15e5dff5a607e4e091c29bafd65bc3e567b94
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-11-28 19:23:19 +0900

    Update BaseController

--- wordpress/html/wp-content/plugins/hskwakr-practice/inc/Base/BaseController.php
+++ wordpress/html/wp-content/plugins/hskwakr-practice/inc/Base/BaseController.php
@@ -12,14 +12,14 @@ abstract class BaseController
 {
   public $plugin_file;
   public $plugin_path;
+  public $plugin_name;
   public $plugin_url;
 
   public function __construct()
   {
     $this->plugin_path = plugin_dir_path( dirname( __FILE__, 2 ) );
     $this->plugin_url  = plugin_dir_url( dirname( __FILE__, 2 ) );
-
-    $name = plugin_basename( dirname( __FILE__, 3 ) );
-    $this->plugin_file = $name . '/' . $name . '.php';
+    $this->plugin_name = plugin_basename( dirname( __FILE__, 3 ) );
+    $this->plugin_file = $this->plugin_name . '/' . $this->plugin_name . '.php';
   }
 }
