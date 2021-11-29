94e0492c6ca79b3812cdd3965914fdc4a47d542a
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-11-29 17:27:19 +0900

    Update Enqueue

--- wordpress/html/wp-content/plugins/hskwakr-practice/inc/Base/Enqueue.php
+++ wordpress/html/wp-content/plugins/hskwakr-practice/inc/Base/Enqueue.php
@@ -14,13 +14,13 @@ class Enqueue extends BaseController
 {
   public function register()
   {
-    add_action( 'admin_enque_scripts', array( $this, 'enqueue' ) );
+    add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
   }
 
   function enqueue()
   {
     // enqueue all our scripts
-    wp_enqueue_style( 'mypluginstyle', $this->plugin_url . '/assets/mystyle.css' );
+    wp_enqueue_style( 'mypluginstyle', $this->plugin_url . 'assets/mystyle.css' );
     wp_enqueue_script( 'mypluginscript', $this->plugin_url . 'assets/myscript.js' );
   }
 }
