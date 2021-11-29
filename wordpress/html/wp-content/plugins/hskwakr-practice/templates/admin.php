522a13d6a044a498839f15e3d5e356eea522cfab
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-11-29 20:14:28 +0900

    Update admin page

--- wordpress/html/wp-content/plugins/hskwakr-practice/templates/admin.php
+++ wordpress/html/wp-content/plugins/hskwakr-practice/templates/admin.php
@@ -4,15 +4,15 @@
 
   <ul class="nav nav-tabs">
     <li class="active">
-      <a href="tab-1">Manage Settings</a>
+      <a href="#tab-1">Manage Settings</a>
     </li>
 
     <li>
-      <a href="tab-2">Updates</a>
+      <a href="#tab-2">Updates</a>
     </li>
 
     <li>
-      <a href="tab-3">About</a>
+      <a href="#tab-3">About</a>
     </li>
   </ul>
 
@@ -27,11 +27,11 @@
       </form>
     </div>
 
-    <div id="tab-2">
+    <div id="tab-2" class="tab-pane">
       <h3>Updates</h3>
     </div>
 
-    <div id="tab-3">
+    <div id="tab-3" class="tab-pane">
       <h3>About</h3>
     </div>
   </div>
