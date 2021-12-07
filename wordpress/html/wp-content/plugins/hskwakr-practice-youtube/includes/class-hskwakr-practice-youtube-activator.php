9910e0cf12d011cc7095ef2b1ca4105e0bd6a34e
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-12-08 01:23:34 +0900

    Add new practice plagin

---
+++ wordpress/html/wp-content/plugins/hskwakr-practice-youtube/includes/class-hskwakr-practice-youtube-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/hskwakr/wp-plugin-practice
+ * @since      1.0.0
+ *
+ * @package    Hskwakr_Practice_Youtube
+ * @subpackage Hskwakr_Practice_Youtube/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Hskwakr_Practice_Youtube
+ * @subpackage Hskwakr_Practice_Youtube/includes
+ * @author     hskwakr <33633391+hskwakr@users.noreply.github.com>
+ */
+class Hskwakr_Practice_Youtube_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ wordpress/html/wp-content/plugins/hskwakr-practice-youtube/includes/class-hskwakr-practice-youtube-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/hskwakr/wp-plugin-practice
+ * @since      1.0.0
+ *
+ * @package    Hskwakr_Practice_Youtube
+ * @subpackage Hskwakr_Practice_Youtube/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Hskwakr_Practice_Youtube
+ * @subpackage Hskwakr_Practice_Youtube/includes
+ * @author     hskwakr <33633391+hskwakr@users.noreply.github.com>
+ */
+class Hskwakr_Practice_Youtube_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
