3c83234bf6e196e23b87f587e807743c62054d49
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-12-11 10:56:59 +0900

    Update plugin
    Add custom post type for youtube videos.

--- wordpress/html/wp-content/plugins/hskwakr-practice-youtube/admin/class-hskwakr-practice-youtube-admin.php
+++ wordpress/html/wp-content/plugins/hskwakr-practice-youtube/admin/class-hskwakr-practice-youtube-admin.php
@@ -143,14 +143,82 @@ class Hskwakr_Practice_Youtube_Admin
     }
 
     /**
-     * Add our custom menu.
+     * Register all settings
      *
      * @since    1.0.0
      */
     public function register_my_plugin_general_settings()
     {
-        // registers all settings
         register_setting('hskwakr-practice-youtube-custom-settings', 'hskwakrYoutubeAPIKey');
         register_setting('hskwakr-practice-youtube-custom-settings', 'hskwakrYoutubeChannelId');
+    }
+
+    /**
+     * Create custom post type for youtube videos
+     *
+     * @return void
+     */
+    public function custom_youtube_api($param)
+    {
+        $name = 'videos-hpy';
+        /**
+         * Creating a function to create our CPT
+         */
+        $labels = array(
+          'name'                => _x('YouTube Videos', 'Post Type General Name'),
+          'singular_name'       => _x('YouTube Video', 'Post Type Singular Name'),
+          'menu_name'           => __('YouTube Video'),
+          'parent_item_colon'   => __('Parent Video'),
+          'all_items'           => __('All Videos'),
+          'view_item'           => __('View Videos'),
+          'add_new_item'        => __('Add New YouTube Video'),
+          'add_new'             => __('Add New'),
+          'edit_item'           => __('Edit'),
+          'update_item'         => __('Update'),
+          'search_items'        => __('Search'),
+          'not_found'           => __('Not Found'),
+          'not_found_in_trash'  => __('Not found in Trash'),
+        );
+
+        // Set other options for Custom Post Type
+        $args = array(
+          'label'               => __($name),
+          'description'         => __('YouTube Videos from our Channel'),
+          'labels'              => $labels,
+          // Features this CPT supports in Post Editor
+          'supports'            => array(
+            'title',
+            'editor',
+            'excerpt',
+            'author',
+            'thumbnail',
+            'comments',
+            'revisions',
+            'custom-fields',
+          ),
+          // You can associate this CPT with a taxonomy or custom taxonomy.
+          'taxonomies'          => array('genres'),
+          /**
+           * A hierarchical CPT is like Pages and can have
+           * Parent and child items. A non-hierarchical CPT
+           * is like Posts.
+           */
+          'hierarchical'        => false,
+          'public'              => true,
+          'show_ui'             => false,
+          'show_in_menu'        => false,
+          'show_in_nav_menus'   => true,
+          'show_in_admin_bar'   => true,
+          'menu_position'       => 5,
+          'can_export'          => false,
+          'has_archive'         => true,
+          'exclude_from_search' => true,
+          'publicly_queryable'  => true,
+          'capability_type'     => 'post',
+          'show_in_rest'        => true,
+        );
+
+        // Registering your Custom Post Type
+        register_post_type($name, $args);
     }
 }
